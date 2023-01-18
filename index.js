86bae5c6b3f952aca78fe792c143c5b18cea34d6
Adamu Jethro <45628811+JayWebtech@users.noreply.github.com>
2023-01-18 07:59:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get("/", (req, res) => {
     try{
         const state = req.query.state;
         for (let index = 0; index < Object.keys(lgaList).length; index++) {
-            res.status(200).json({Object.values(lgaList.lgaList[state])});
-            //res.send(Object.values(lgaList.lgaList[state]));
+            //res.status(200).json({Object.values(lgaList.lgaList[state])});
+            res.status(200).send(Object.values(lgaList.lgaList[state]));
         } 
     }catch (err){
         res.send("Error: Inavalid state name");
