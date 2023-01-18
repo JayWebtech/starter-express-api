6d49822006c2d8af04ff645d19f7457bdfc7a7b1
Adamu Jethro <45628811+JayWebtech@users.noreply.github.com>
2023-01-18 07:53:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
+const express = require("express");
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const server = require("http").Server(app)
+const lgaList = require("./lga");
+
+app.get("/", (req, res) => {
+    try{
+        const state = req.query.state;
+        for (let index = 0; index < Object.keys(lgaList).length; index++) {
+            res.send(Object.values(lgaList.lgaList[state]));
+        } 
+    }catch (err){
+        res.send("Error: Make sure the state name is correct");
+    }
+});
+
+
+app.use((req, res) => {
+    res.status(404);
+});
+server.listen(process.env.PORT || 3000);
